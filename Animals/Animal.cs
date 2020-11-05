a870aab3db6ada1b56d0f17b2755796166556ad1
Remy Ghalayini <remyghalayini@gmail.com>
2020-11-05 10:29:50 +0100

    first commit

---
+++ Animals/Animal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public string FavouriteFood { get; set; }
+
+        protected Animal (string name, string favouriteFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favouriteFood;
+        }
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my favourite food is {this.FavouriteFood}";
+        }
+    }
+
+    class Cat :Animal
+    {
+        public Cat(string name, string favouriteFood) : base(name, favouriteFood)
+        { }
+        public override string ExplainSelf()
+        {
+            return base.ExplainSelf() + Environment.NewLine + "MEEOW";
+        }
+    }
+    class Dog : Animal
+    {
+        public Dog (string name, string favouriteFood) : base(name, favouriteFood)
+        { }
+        public override string ExplainSelf()
+        {
+            return base.ExplainSelf() + Environment.NewLine + "WOUF";
+        }
+
+    }
+}
---
+++ Animals/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Cat("Peter", "Whiskas");
+            Animal dog = new Dog("Gunnar", "Meat");
+
+            Console.WriteLine(cat.ExplainSelf());
+            Console.WriteLine(dog.ExplainSelf());
+            Console.ReadLine();
+        }
+    }
+}
